ec53658fd9238223a7681a2487248295721e8c96
Derek Slater <derekslater01@gmail.com>
2021-05-23 15:43:23 -0700

    Some minor projectile changes

--- Assets/Scripts/Mechanics/playerProjectile.cs
+++ Assets/Scripts/Mechanics/playerProjectile.cs
@@ -18,7 +18,7 @@ public class playerProjectile : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Ground")
+        if (collision.gameObject.tag != "Player")
         {
             Destroy(gameObject);
         }
